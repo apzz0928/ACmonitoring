d837ffa8e214fb539a3055f64081f767f52fb4ed
apzz0928 <apzz0928@gmail.com>
2019-12-30 09:00:49 +0900

    2019/12/30 : 방문자 검색 날짜 변경

--- src/test/java/aceCounter/CPCInflow.java
+++ src/test/java/aceCounter/CPCInflow.java
@@ -18,6 +18,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.codeborne.selenide.*;
 import com.codeborne.selenide.WebDriverRunner;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -525,6 +526,6 @@ public class CPCInflow {
   	}
 	@AfterClass
 	public void afterTest() {
-		closeWebDriver();
+		close();
 	}
 }
